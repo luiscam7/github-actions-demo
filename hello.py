f18e4beaffecba56101b69fc5ffe691bab052055
Luis Camargo <lcamargo2007@gmail.com>
2023-05-04 21:29:56 +0000

    added subtract function and test function

--- hello.py
+++ hello.py
@@ -3,5 +3,14 @@ def add(x, y):
 
     return x + y
 
+def subtract(x, y):
+    """This is an add function"""
+
+    return x - y
+
 
 print(add(1, 1))
+
+print(subtract(7, 1))
+
+
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,8 @@
-from hello import add
+from hello import add, subtract
 
 
 def test_add():
     assert 2 == add(1, 1)
+
+def test_subtract():
+    assert 3 == subtract(7,4)
