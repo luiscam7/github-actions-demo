f9ac286cc9f0ec74589ad3f2b8344e33c238c96b
Luis Camargo <lcamargo2007@gmail.com>
2023-05-04 21:37:41 +0000

    new division function

--- hello.py
+++ hello.py
@@ -3,14 +3,19 @@ def add(x, y):
 
     return x + y
 
+
 def subtract(x, y):
     """This is an subtraction function"""
 
     return x - y
 
 
+def divide(x, y):
+    """This is an divide function"""
+
+    return x / y
+
+
 print(add(1, 1))
 
 print(subtract(7, 1))
-
-
--- test_hello.py
+++ test_hello.py
@@ -1,8 +1,13 @@
-from hello import add, subtract
+from hello import add, subtract, divide
 
 
 def test_add():
     assert 2 == add(1, 1)
 
+
 def test_subtract():
-    assert 3 == subtract(7,4)
+    assert 3 == subtract(7, 4)
+
+
+def test_divide():
+    assert 2 == divide(8, 4)
